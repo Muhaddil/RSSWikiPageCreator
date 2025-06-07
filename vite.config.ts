c87a6a6e77f8b23833c340c4c8f33eaf68150534
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2025-06-08 00:38:38 +0200

    fix: remove manual chunking configuration from rollup options in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,14 +9,6 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-          return null;
-        },
-      },
       input: [
         'index.html',
         'indextest.html',
