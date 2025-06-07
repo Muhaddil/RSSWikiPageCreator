1b14825d2cadfb867a02b310a5ef2ca9aac3fe91
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2025-06-08 00:04:19 +0200

    fix: update version numbers to 1.0.6241 in package.json, package-lock.json, and version.json; add new web update entry in Cronology.vue

--- src/pages/Cronology.vue
+++ src/pages/Cronology.vue
@@ -62,6 +62,12 @@ const events = ref<TimelineEvent[]>([
   // Web Updates
 
   {
+    title: '1.0.6241',
+    date: '08 / 06 / 2025',
+    description: 'Se ha añadido un nuevo parametro faltante para el sistema de minificado de la web al momento de subirse a GHub Pages.',
+    category: 'Web',
+  },
+  {
     title: '1.0.624',
     date: '08 / 06 / 2025',
     description: 'Se ha sustituido el sistema de minificado de la web a la hora de subirse a GHub Pages.',
