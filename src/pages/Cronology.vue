391461371c38a969a81abd72f51646fe81d02f6b
Muhaddil <markellur08@gmail.com>
2025-08-07 11:01:15 +0200

    chore: Bump version to 1.0.645 and update changelog in Cronology.vue

--- src/pages/Cronology.vue
+++ src/pages/Cronology.vue
@@ -62,6 +62,12 @@ const events = ref<TimelineEvent[]>([
   // Web Updates
 
   {
+    title: '1.0.645',
+    date: '07 / 08 / 2025',
+    description: 'Se ha actualizado una dependencia a su última versión (vite).',
+    category: 'Web',
+  },
+  {
     title: '1.0.644',
     date: '05 / 08 / 2025',
     description: 'Se ha actualizado una dependencia a su última versión (node).',
