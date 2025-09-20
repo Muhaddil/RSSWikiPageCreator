0c6944dc09b3c6a07d49a3d149a57580ad1edaf0
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2025-09-20 13:14:14 +0200

    chore: update version to 1.0.669 in package.json, package-lock.json, and version.json; add release note in Cronology.vue

--- src/pages/Cronology.vue
+++ src/pages/Cronology.vue
@@ -62,6 +62,12 @@ const events = ref<TimelineEvent[]>([
   // Web Updates
 
   {
+    title: '1.0.669',
+    date: '20 / 09 / 2025',
+    description: 'Se han actualizado varias dependencias de la web.',
+    category: 'Web',
+  },
+  {
     title: '1.0.668',
     date: '14 / 09 / 2025',
     description: 'Se han actualizado dos dependencias de la web.',
