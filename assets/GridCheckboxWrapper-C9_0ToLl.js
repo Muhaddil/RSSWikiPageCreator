202c9e0d2048f2fa0a7ffa25ff62bf388edeedec
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2026-07-12 16:36:28 +0000

    Deploying to gh-pages from @ Muhaddil/RSSWikiPageCreator@ea89d8d0bb43872804f331b3deb6b4d6d9fd3f45 🚀

---
+++ assets/GridCheckboxWrapper-C9_0ToLl.js
@@ -0,0 +1,88 @@
+import{C as e,H as t,I as n,J as r,L as i,N as a,P as o,Pt as s,S as c,c as l,ft as u,g as d,h as f,j as p,jt as m,l as h,o as g,q as _,r as v,s as y,u as b}from"./runtime-core.esm-bundler-DdnIPB3D.js";import{s as x,t as S}from"./runtime-dom.esm-bundler-LkOBghAH.js";import{a as C}from"./SanitisedTextInput-CFvHORas.js";import{t as w}from"./_plugin-vue_export-helper-BDNMzG2s.js";import{c as T,it as E,l as D,n as O}from"./button-cKZorYEn.js";import{n as k,t as A}from"./minus-DSknPgJF.js";import{t as j}from"./checkbox-fgmS5IDq.js";var M=D.extend({name:`fieldset`,style:`
+    .p-fieldset {
+        background: dt('fieldset.background');
+        border: 1px solid dt('fieldset.border.color');
+        border-radius: dt('fieldset.border.radius');
+        color: dt('fieldset.color');
+        padding: dt('fieldset.padding');
+        margin: 0;
+    }
+
+    .p-fieldset-legend {
+        background: dt('fieldset.legend.background');
+        border-radius: dt('fieldset.legend.border.radius');
+        border-width: dt('fieldset.legend.border.width');
+        border-style: solid;
+        border-color: dt('fieldset.legend.border.color');
+        color: dt('fieldset.legend.color');
+        padding: dt('fieldset.legend.padding');
+        transition:
+            background dt('fieldset.transition.duration'),
+            color dt('fieldset.transition.duration'),
+            outline-color dt('fieldset.transition.duration'),
+            box-shadow dt('fieldset.transition.duration');
+    }
+
+    .p-fieldset-toggleable > .p-fieldset-legend {
+        padding: 0;
+    }
+
+    .p-fieldset-toggle-button {
+        cursor: pointer;
+        user-select: none;
+        overflow: hidden;
+        position: relative;
+        text-decoration: none;
+        display: flex;
+        gap: dt('fieldset.legend.gap');
+        align-items: center;
+        justify-content: center;
+        padding: dt('fieldset.legend.padding');
+        background: transparent;
+        border: 0 none;
+        border-radius: dt('fieldset.legend.border.radius');
+        transition:
+            background dt('fieldset.transition.duration'),
+            color dt('fieldset.transition.duration'),
+            outline-color dt('fieldset.transition.duration'),
+            box-shadow dt('fieldset.transition.duration');
+        outline-color: transparent;
+    }
+
+    .p-fieldset-legend-label {
+        font-weight: dt('fieldset.legend.font.weight');
+    }
+
+    .p-fieldset-toggle-button:focus-visible {
+        box-shadow: dt('fieldset.legend.focus.ring.shadow');
+        outline: dt('fieldset.legend.focus.ring.width') dt('fieldset.legend.focus.ring.style') dt('fieldset.legend.focus.ring.color');
+        outline-offset: dt('fieldset.legend.focus.ring.offset');
+    }
+
+    .p-fieldset-toggleable > .p-fieldset-legend:hover {
+        color: dt('fieldset.legend.hover.color');
+        background: dt('fieldset.legend.hover.background');
+    }
+
+    .p-fieldset-toggle-icon {
+        color: dt('fieldset.toggle.icon.color');
+        transition: color dt('fieldset.transition.duration');
+    }
+
+    .p-fieldset-toggleable > .p-fieldset-legend:hover .p-fieldset-toggle-icon {
+        color: dt('fieldset.toggle.icon.hover.color');
+    }
+
+    .p-fieldset-content-container {
+        display: grid;
+        grid-template-rows: 1fr;
+    }
+
+    .p-fieldset-content-wrapper {
+        min-height: 0;
+    }
+
+    .p-fieldset-content {
+        padding: dt('fieldset.content.padding');
+    }
+`,classes:{root:function(e){return[`p-fieldset p-component`,{"p-fieldset-toggleable":e.props.toggleable}]},legend:`p-fieldset-legend`,legendLabel:`p-fieldset-legend-label`,toggleButton:`p-fieldset-toggle-button`,toggleIcon:`p-fieldset-toggle-icon`,contentContainer:`p-fieldset-content-container`,contentWrapper:`p-fieldset-content-wrapper`,content:`p-fieldset-content`}}),N={name:`Fieldset`,extends:{name:`BaseFieldset`,extends:T,props:{legend:String,toggleable:Boolean,collapsed:Boolean,toggleButtonProps:{type:null,default:null}},style:M,provide:function(){return{$pcFieldset:this,$parentInstance:this}}},inheritAttrs:!1,emits:[`update:collapsed`,`toggle`],data:function(){return{d_collapsed:this.collapsed}},watch:{collapsed:function(e){this.d_collapsed=e}},methods:{toggle:function(e){this.d_collapsed=!this.d_collapsed,this.$emit(`update:collapsed`,this.d_collapsed),this.$emit(`toggle`,{originalEvent:e,value:this.d_collapsed})},onKeyDown:function(e){(e.code===`Enter`||e.code===`NumpadEnter`||e.code===`Space`)&&(this.toggle(e),e.preventDefault())}},computed:{buttonAriaLabel:function(){return this.toggleButtonProps&&this.toggleButtonProps.ariaLabel?this.toggleButtonProps.ariaLabel:this.legend},dataP:function(){return E({toggleable:this.toggleable})}},directives:{ripple:O},components:{PlusIcon:k,MinusIcon:A}};function P(e){"@babel/helpers - typeof";return P=typeof Symbol==`function`&&typeof Symbol.iterator==`symbol`?function(e){return typeof e}:function(e){return e&&typeof Symbol==`function`&&e.constructor===Symbol&&e!==Symbol.prototype?`symbol`:typeof e},P(e)}function F(e,t){var n=Object.keys(e);if(Object.getOwnPropertySymbols){var r=Object.getOwnPropertySymbols(e);t&&(r=r.filter(function(t){return Object.getOwnPropertyDescriptor(e,t).enumerable})),n.push.apply(n,r)}return n}function I(e){for(var t=1;t<arguments.length;t++){var n=arguments[t]==null?{}:arguments[t];t%2?F(Object(n),!0).forEach(function(t){L(e,t,n[t])}):Object.getOwnPropertyDescriptors?Object.defineProperties(e,Object.getOwnPropertyDescriptors(n)):F(Object(n)).forEach(function(t){Object.defineProperty(e,t,Object.getOwnPropertyDescriptor(n,t))})}return e}function L(e,t,n){return(t=R(t))in e?Object.defineProperty(e,t,{value:n,enumerable:!0,configurable:!0,writable:!0}):e[t]=n,e}function R(e){var t=z(e,`string`);return P(t)==`symbol`?t:t+``}function z(e,t){if(P(e)!=`object`||!e)return e;var n=e[Symbol.toPrimitive];if(n!==void 0){var r=n.call(e,t);if(P(r)!=`object`)return r;throw TypeError(`@@toPrimitive must return a primitive value.`)}return(t===`string`?String:Number)(e)}var B=[`data-p`],V=[`data-p`],H=[`id`],U=[`id`,`aria-controls`,`aria-expanded`,`aria-label`],W=[`id`,`aria-labelledby`];function G(t,a,c,u,d,g){var v=n(`ripple`);return p(),b(`fieldset`,e({class:t.cx(`root`),"data-p":g.dataP},t.ptmi(`root`)),[y(`legend`,e({class:t.cx(`legend`),"data-p":g.dataP},t.ptm(`legend`)),[o(t.$slots,`legend`,{toggleCallback:g.toggle},function(){return[t.toggleable?h(``,!0):(p(),b(`span`,e({key:0,id:t.$id+`_header`,class:t.cx(`legendLabel`)},t.ptm(`legendLabel`)),s(t.legend),17,H)),t.toggleable?r((p(),b(`button`,e({key:1,id:t.$id+`_header`,type:`button`,"aria-controls":t.$id+`_content`,"aria-expanded":!d.d_collapsed,"aria-label":g.buttonAriaLabel,class:t.cx(`toggleButton`),onClick:a[0]||=function(){return g.toggle&&g.toggle.apply(g,arguments)},onKeydown:a[1]||=function(){return g.onKeyDown&&g.onKeyDown.apply(g,arguments)}},I(I({},t.toggleButtonProps),t.ptm(`toggleButton`))),[o(t.$slots,t.$slots.toggleicon?`toggleicon`:`togglericon`,{collapsed:d.d_collapsed,class:m(t.cx(`toggleIcon`))},function(){return[(p(),l(i(d.d_collapsed?`PlusIcon`:`MinusIcon`),e({class:t.cx(`toggleIcon`)},t.ptm(`toggleIcon`)),null,16,[`class`]))]}),y(`span`,e({class:t.cx(`legendLabel`)},t.ptm(`legendLabel`)),s(t.legend),17)],16,U)),[[v]]):h(``,!0)]})],16,V),f(S,e({name:`p-collapsible`},t.ptm(`transition`)),{default:_(function(){return[r(y(`div`,e({id:t.$id+`_content`,class:t.cx(`contentContainer`),role:`region`,"aria-labelledby":t.$id+`_header`},t.ptm(`contentContainer`)),[y(`div`,e({class:t.cx(`contentWrapper`)},t.ptm(`contentWrapper`)),[y(`div`,e({class:t.cx(`content`)},t.ptm(`content`)),[o(t.$slots,`default`)],16)],16)],16,W),[[x,!d.d_collapsed]])]}),_:3},16)],16,B)}N.render=G;var K={class:`is-flex is-align-items-end is-gap-1 is-flex-grow-1`},q=[`for`],J=d({__name:`CompactCheckbox`,props:c({label:{},trueValue:{},falseValue:{},value:{},name:{}},{modelValue:{required:!0},modelModifiers:{}}),emits:[`update:modelValue`],setup(e){let n=t(e,`modelValue`),r=C(`checkbox-`);return(t,i)=>(p(),b(`div`,K,[f(u(j),{modelValue:n.value,"onUpdate:modelValue":i[0]||=e=>n.value=e,"false-value":e.falseValue,"input-id":u(r),"true-value":e.trueValue,binary:``},null,8,[`modelValue`,`false-value`,`input-id`,`true-value`]),y(`label`,{for:u(r),class:`is-flex-grow-1`},s(e.label),9,q)]))}});function Y(e,t){return e.reduce((e,n,r)=>{let i=Math.floor(r/t);return e[i]||(e[i]=[]),e[i].push(n),e},[])}var X={class:`columns is-mobile mb-0 checkbox-row`},Z=`Yes`,Q=`No`,$=w(d({__name:`GridCheckboxWrapper`,props:{checkboxes:{}},setup(e){let t=e,n=g(()=>Y(t.checkboxes,2));return(e,t)=>(p(!0),b(v,null,a(n.value,e=>(p(),b(`div`,X,[(p(!0),b(v,null,a(e,e=>(p(),b(`div`,{key:e.label,class:`column is-flex is-align-items-center checkbox-wrapper`},[f(J,{modelValue:e.model,"onUpdate:modelValue":t=>e.model=t,"false-value":e.falseValue??Q,label:e.label,"true-value":e.trueValue??Z},null,8,[`modelValue`,`onUpdate:modelValue`,`false-value`,`label`,`true-value`])]))),128))]))),256))}}),[[`__scopeId`,`data-v-7542eed2`]]);export{N as n,$ as t};
